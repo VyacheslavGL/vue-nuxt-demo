93583b7c65eaae29837b916c4c3a8065c3404b2a
liliaretsia <retsi3247044>
2019-11-26 21:58:38 +0300

    Initial commit

---
+++ layouts/error.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <h1 v-if="error.statusCode === 404">Ошибка 404</h1>
+        <h1 v-else>Что-то пошло не так</h1>
+        <nuxt-link to="/about">О компании</nuxt-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "error",
+        props: {
+            error: {
+                type: Object,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ store/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Vuex from 'Vuex';
+
+Vue.use(Vuex);
+
+export default function () {
+    return new Vuex.Store({
+        state: {
+            test: 'Hello',
+        },
+        mutations: {
+            SET_TEST(state, value) {
+                state.test = value;
+            }
+        },
+        actions: {
+            async GET_TEST({ state }) {
+                return state.test
+            },
+            async LOAD_TEST({commit}){
+                const words = ['Test', 'something', 'else'];
+                commit('SET_TEST', words[~~(Math.random() * words.length)]);
+            }
+        }
+    });
+}
